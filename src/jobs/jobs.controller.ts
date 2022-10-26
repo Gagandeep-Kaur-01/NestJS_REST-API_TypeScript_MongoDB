290178c29c9ff17a177eb53777d39b6469fe0b75
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-10-26 12:43:13 +0530

    creating api's

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
 @Controller('jobs')
-export class JobsController {}
+
+export class JobsController {
+
+    @Get()
+    findAll(): string {
+        return '--findAll--';
+    }
+
+    @Post()
+    create(): string {
+        return '--create--'
+    }
+
+    @Put()
+    update(): string {
+        return '--update----'
+    }
+
+    @Delete()
+    delete(): string {
+        return '---delete---'
+    }
+
+}
