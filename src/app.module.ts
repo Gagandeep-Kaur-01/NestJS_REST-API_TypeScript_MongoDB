c782db831aa6f07f751ea2d070b0b81342e3a8b0
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-10-26 16:11:54 +0530

    updated code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JobsController } from './jobs/jobs.controller';
 import { JobsService } from './jobs/jobs.service';
+import { JobsModule } from './jobs/jobs.module';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [JobsModule, MongooseModule.forRoot('mongodb://localhost:27017/jobs')],
   controllers: [AppController, JobsController],
   providers: [AppService, JobsService],
 })
