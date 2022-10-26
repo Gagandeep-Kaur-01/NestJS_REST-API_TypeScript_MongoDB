3651b87bea6b9387702e1d5227d444e0fa3fdcb1
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-10-26 15:54:19 +0530

    minor correction

--- src/jobs/job.module.ts
+++ src/jobs/job.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JobsController } from './jobs.controller';
 import { JobsService } from './jobs.service';
+import { JobSchema } from './schemas/job.schema'
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: 'Job', schema: JobSchema }])],
   controllers: [JobsController],
   providers: [JobsService],
 })
