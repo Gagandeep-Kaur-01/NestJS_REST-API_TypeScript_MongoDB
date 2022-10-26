87d406c3ff3813f09e0efb876c842f9ceeef926b
Gagandeep Kaur <gagandeep07kaur@gmail.com>
2022-10-26 13:06:04 +0530

    created service file - nest g service jobs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JobsController } from './jobs/jobs.controller';
+import { JobsService } from './jobs/jobs.service';
 
 @Module({
   imports: [],
   controllers: [AppController, JobsController],
-  providers: [AppService],
+  providers: [AppService, JobsService],
 })
 export class AppModule {}
